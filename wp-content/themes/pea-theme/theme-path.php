278c3063c702bda44c75e1a67ca00e8cc587dc43
Andrea Gasparini <info@ideazero.it>
2019-11-12 12:33:46 +0100

    Initial Commit

---
+++ wp-content/themes/pea-theme/theme-path.php
@@ -0,0 +1,9 @@
+<?php
+/** Define constants */
+define( 'THEME_ROOT', get_stylesheet_directory_uri() );
+define( 'THEME_ROOT_DIR', get_stylesheet_directory() );
+define( 'THEME_UPLOAD_DIR', wp_get_upload_dir() );
+define( 'THEME_ASSETS', THEME_ROOT_DIR . '/src/assets' );
+define( 'THEME_CSS', THEME_ROOT . '/css' );
+define( 'THEME_JS', THEME_ROOT . '/js' );
+define( 'THEME_SVG', THEME_ROOT . '/svg' );
